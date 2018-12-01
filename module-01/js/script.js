f89ca90fa53d03db90c5f2e0a3d0819aa5a56910
zayac <zaicevvv1991@gmail.com>
2018-12-01 11:26:43 +0200

    m1dop

--- module-01/js/script.js
+++ module-01/js/script.js
@@ -1,23 +1,72 @@
 'use strict';
-const adminLogin = 'admin';
-const adminPassword = 'm4ng0h4ckz';
-const cansel = 'Отменено пользователем!';
-const wrongLogin = 'Доступ запрещен, неверный логин!';
-const wrongPassword = 'Доступ запрещен, неверный пароль!';
-const welcome = 'Добро пожаловать!';
-const inputLogin = prompt('Введите логин');
-let inputPassword;
-if (!inputLogin) {
+// const adminLogin = 'admin';
+// const adminPassword = 'password';
+// const cansel = 'Отменено пользователем!';
+// const wrongLogin = 'Доступ запрещен, неверный логин!';
+// const wrongPassword = 'Доступ запрещен, неверный пароль!';
+// const welcome = 'Добро пожаловать!';
+// const inputLogin = prompt('Введите логин');
+// let inputPassword;
+// if (!inputLogin) {
+//   alert(cansel);
+// } else if (inputLogin !== adminLogin) {
+//   alert(wrongLogin);
+// } else {
+//   inputPassword = prompt('Введите пароль');
+//   if (!inputPassword) {
+//     alert(cansel);
+//   } else if (inputPassword !== adminPassword) {
+//     alert(wrongPassword);
+//   } else {
+//     alert(welcome);
+//   }
+// }
+
+// Дополнительное задание
+
+const sharm = 15;
+const hurgada = 25;
+const taba = 6;
+const cansel = 'Нам очень жаль, приходите еще!';
+let userInput = prompt('Введите число необходимых мест');
+let userConfirm;
+if (!userInput) {
   alert(cansel);
-} else if (inputLogin !== adminLogin) {
-  alert(wrongLogin);
 } else {
-  inputPassword = prompt('Введите пароль');
-  if (!inputPassword) {
-    alert(cansel);
-  } else if (inputPassword !== adminPassword) {
-    alert(wrongPassword);
+  userInput = Number(userInput);
+  if (userInput % 1 === 0 && userInput > 0) {
+    if (userInput <= sharm) {
+      userConfirm = confirm(
+        'Есть места в группе Sharm, хотите присоединиться?',
+      );
+      if (userConfirm === true) {
+        alert('Приятного путешествия в группе Sharm');
+      } else {
+        alert(cansel);
+      }
+    }
+    if (userInput <= hurgada) {
+      userConfirm = confirm(
+        'Есть места в группе Hurgada, хотите присоединиться?',
+      );
+      if (userConfirm === true) {
+        alert('Приятного путешествия в группе Hurgada');
+      } else {
+        alert(cansel);
+      }
+    }
+    if (userInput <= taba) {
+      userConfirm = confirm('Есть места в группе Taba, хотите присоединиться?');
+      if (userConfirm === true) {
+        alert('Приятного путешествия в группе Taba');
+      } else {
+        alert(cansel);
+      }
+    }
+    if (userInput > 25) {
+      alert('Извините, столько мест нет ни в одной группе!');
+    }
   } else {
-    alert(welcome);
+    alert('Ошибка ввода!');
   }
 }
