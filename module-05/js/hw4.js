b1481766e15a716abab478c5e9558576dc10e11d
zayac <zaicevvv1991@gmail.com>
2019-01-12 20:00:14 +0200

    hw5

---
+++ module-05/js/hw4.js
@@ -0,0 +1,225 @@
+'use strict';
+
+/*
+ Note Schema
+id: string | integer
+title: string
+body: string
+priority: integer [0-2]
+*/
+
+const PRIORITY_TYPES = {
+  LOW: 0,
+  NORMAL: 1,
+  HIGH: 2,
+};
+
+// Notepad Model
+const notepad = {
+  notes: [],
+  getNotes() {
+    return this.notes;
+    /*
+     * Принимает: ничего
+     * Возвращает: все заметки, значение свойства notes
+     */
+  },
+  findNoteById(id) {
+    for (const note of this.notes) {
+      if (note.id === id) return note;
+    }
+    /*
+     * Ищет заметку в массиве notes
+     *
+     * Принимает: идентификатор заметки
+     * Возвращает: заметку с совпавшим полем id или undefined если ничего не найдено
+     */
+  },
+  saveNote(note) {
+    this.notes.push(note);
+    return note;
+    /*
+     * Сохраняет заметку в массив notes
+     *
+     * Принимает: объект заметки
+     * Возвращает: сохраненную заметку
+     */
+  },
+  deleteNote(id) {
+    for (let i = 0; i < this.notes.length; i += 1) {
+      const note = this.notes[i];
+
+      if (note.id === id) {
+        this.notes.splice(i, 1);
+        return;
+      }
+    }
+    /*
+     * Удаляет заметку по идентификатору из массива notes
+     *
+     * Принимает: идентификатор заметки
+     * Возвращает: ничего
+     */
+  },
+  updateNoteContent(id, updatedContent) {
+    const note = this.findNoteById(id);
+    if (!note) return;
+    const keys = Object.keys(updatedContent);
+    for (const key of keys) {
+      note[key] = updatedContent[key];
+    }
+    return note;
+    /*
+     * Обновляет контент заметки
+     * updatedContent - объект с полями вида {имя: значение, имя: значение}
+     * Свойств в объекте updatedContent может быть произвольное количество
+     *
+     * Принимает: идентификатор заметки и объект, полями которого надо обновить заметку
+     * Возвращает: обновленную заметку
+     */
+  },
+  updateNotePriority(id, priority) {
+    const note = this.findNoteById(id);
+    if (!note) return;
+    note.priority = priority;
+    return note;
+    /*
+     * Обновляет приоритет заметки
+     *
+     * Принимает: идентификатор заметки и ее новый приоритет
+     * Возвращает: обновленную заметку
+     */
+  },
+  filterNotesByQuery(query) {
+    const filteredNotes = [];
+    for (const note of this.notes) {
+      const { title, body } = note;
+      const noteContent = `${title} ${body}`;
+      const hasQuery = noteContent.toLowerCase().includes(query.toLowerCase());
+      if (hasQuery) {
+        filteredNotes.push(note);
+      }
+    }
+    return filteredNotes;
+    /*
+     * Фильтрует массив заметок по подстроке query.
+     * Если значение query есть в заголовке или теле заметки - она подходит
+     *
+     * Принимает: подстроку для поиска в title и body заметки
+     * Возвращает: новый массив заметок, контент которых содержит подстроку
+     */
+  },
+  filterNotesByPriority(priority) {
+    const filteredNotes = [];
+    for (const note of this.notes) {
+      if (note.priority === priority) {
+        filteredNotes.push(note);
+      }
+    }
+    return filteredNotes;
+    /*
+     * Фильтрует массив заметок по значению приоритета
+     * Если значение priority совпадаем с приоритетом заметки - она подходит
+     *
+     * Принимает: приоритет для поиска в свойстве priority заметки
+     * Возвращает: новый массив заметок с подходящим приоритетом
+     */
+  },
+};
+
+/*
+ * Добавляю 4 заметки и смотрю что получилось
+ */
+notepad.saveNote({
+  id: 1,
+  title: 'JavaScript essentials',
+  body:
+    'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
+  priority: PRIORITY_TYPES.HIGH,
+});
+
+notepad.saveNote({
+  id: 2,
+  title: 'Refresh HTML and CSS',
+  body:
+    'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
+  priority: PRIORITY_TYPES.NORMAL,
+});
+
+notepad.saveNote({
+  id: 3,
+  title: 'Get comfy with Frontend frameworks',
+  body:
+    'First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
+  priority: PRIORITY_TYPES.NORMAL,
+});
+
+notepad.saveNote({
+  id: 4,
+  title: 'Winter clothes',
+  body:
+    "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+  priority: PRIORITY_TYPES.LOW,
+});
+
+console.log('Все текущие заметки: ', notepad.getNotes());
+// console.log(notepad.saveNote(123));
+/*
+ * Зима уже близко, пора поднять приоритет на покупку одежды
+ */
+notepad.updateNotePriority(4, PRIORITY_TYPES.NORMAL);
+// Смотрю что у меня в заметках
+console.log(
+  'Заметки после обновления приоритета для id 4: ',
+  notepad.getNotes(),
+);
+
+/*
+ * Решил что фреймворки отложу немного, понижаю приоритет
+ */
+notepad.updateNotePriority(3, PRIORITY_TYPES.LOW);
+console.log(
+  'Заметки после обновления приоритета для id 3: ',
+  notepad.getNotes(),
+);
+
+/*
+ * Решил отфильтровать заметки по слову html
+ */
+console.log(
+  'Отфильтровали заметки по ключевому слову "html": ',
+  notepad.filterNotesByQuery('html'),
+);
+
+/*
+ * Решил отфильтровать заметки по слову javascript
+ */
+console.log(
+  'Отфильтровали заметки по ключевому слову "javascript": ',
+  notepad.filterNotesByQuery('javascript'),
+);
+
+/*
+ * Хочу посмотреть только заметки с нормальным приоритетом
+ */
+console.log(
+  'Отфильтровали заметки по нормальному приоритету: ',
+  notepad.filterNotesByPriority(PRIORITY_TYPES.NORMAL),
+);
+
+/*
+ * Обновим контент заметки с id 3
+ */
+notepad.updateNoteContent(3, {
+  title: 'Get comfy with React.js or Vue.js',
+});
+console.log(
+  'Заметки после обновления контента заметки с id 3: ',
+  notepad.getNotes(),
+);
+
+/*
+ * Повторил HTML и CSS, удаляю запись c id 2
+ */
+notepad.deleteNote(2);
+console.log('Заметки после удаления с id 2: ', notepad.getNotes());
--- module-05/js/practice.js
+++ module-05/js/practice.js
@@ -1,1 +1,114 @@
 'use strict';
+
+// level 1
+/*  
+  Напишите код, который бы  с помощью 
+  функции-конструкора User, позволял создавать 
+  объекты пользователя со следующим свойствами:
+    - name - строка (имя)
+    - isActive - буль (активен)
+    - age - число (возраст)
+    - friends - число (кол-во друзей)
+
+  Имя, активность, возраст и друзей, необходимо передать 
+  как аргументы при вызове конструктора.
+  
+  Добавить метод getUserInfo(), которая, выводит строку:
+  `User ${имя} is ${возраст} years old and has ${кол-во друщзей} friends`
+
+  Создать несколько объектов пользователя User и с помощью 
+  функции getUserInfo вывести строку в консоль.
+*/
+
+// const User = function User(
+//   name = 'user',
+//   isActive = 'false',
+//   age = 0,
+//   friends = 0,
+// ) {
+//   this.name = name;
+//   this.isActive = isActive;
+//   this.age = age;
+//   this.friends = friends;
+
+//   this.getUserInfo = function() {
+//     console.log(
+//       `User ${name} is ${age} years old, activity status ${isActive} and has ${friends} friends`,
+//     );
+//   };
+// };
+
+// const user = new User();
+// user.getUserInfo();
+
+// const vova = new User('Vova', true, 27, 10);
+// vova.getUserInfo();
+
+// const mary = new User('Mary', true, 26, 100);
+// mary.getUserInfo();
+
+// level 2
+/*  
+  Расставьте отсутствующие this в методах объекта store
+*/
+
+// const store = {
+//   products: ['bread', 'cheese', 'milk', 'apples'],
+//   managers: ['poly', 'mango', 'ajax'],
+//   addManager(manager) {
+//     this.managers.push(manager);
+
+//     console.log(this.managers);
+//   },
+//   removeManager(manager) {
+//     const idx = this.managers.indexOf(manager);
+
+//     this.managers.splice(idx, 1);
+
+//     console.log(this.managers);
+//   },
+//   getProducts() {
+//     console.log(this.products);
+
+//     return this.products;
+//   },
+// };
+
+// store.addManager('chelsey'); // ['poly', 'mango', 'ajax', 'chelsey']
+
+// store.removeManager('mango'); // ['poly', ajax', 'chelsey']
+
+// store.getProducts(); // ['bread', 'cheese', 'milk', 'apples']
+
+// level 3
+/*  
+  Расставьте отсутствующие this в конструкторе объектов Account
+*/
+
+function Account(login, password, type = 'regular') {
+  this.login = login;
+  this.password = password;
+  this.type = type;
+
+  this.changePassword = function(newPassword) {
+    this.password = newPassword;
+
+    console.log(this.password);
+  };
+
+  this.getAccountInfo = function() {
+    console.log(
+      `Login: ${this.login}, Pass: ${this.password}, Type: ${this.type}`,
+    );
+  };
+}
+
+const account = new Account('Mango', 'qwe123', 'premium');
+
+console.log(account.login); // 'Mango'
+console.log(account.password); // 'qwe123'
+console.log(account.type); // 'premium'
+
+account.changePassword('asdzxc'); // 'asdzxc'
+
+account.getAccountInfo(); // Login: 'Mango', Pass: 'asdzxc', Type: 'premium'
