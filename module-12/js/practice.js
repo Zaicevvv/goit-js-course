9a8a4f9b2242457ab57b322b04ed2f4bbb887390
zayac <zaicevvv1991@gmail.com>
2019-03-24 11:44:05 +0200

    hw12

--- module-12/js/practice.js
+++ module-12/js/practice.js
@@ -1,1 +1,87 @@
 'use strict';
+
+// level 1
+/*
+ * Ознакомься с содержанием панелей HTML и CSS.
+ *
+ * Напиши скрипт который сохраняет выбранную пользователем
+ * тему в localStorage и, при следующих посещениях страницы,
+ * устанавливает ее автоматически. По умолчанию пусть будет светлая тема.
+ *
+ * При выборе темы, добавляй на элемент body соответствующий класс.
+ */
+
+// const body = document.body;
+// const lightBtn = document.querySelector('[data-theme="light"]');
+// const darkBtn = document.querySelector('[data-theme="dark"]');
+
+// body.classList.add(localStorage.getItem('value'));
+
+// const handleLightClick = () => {
+//   body.classList.remove(localStorage.getItem('value'));
+//   localStorage.setItem('value', 'theme-light');
+//   body.classList.add(localStorage.getItem('value'));
+// };
+
+// const handleDarkClick = () => {
+//   body.classList.remove(localStorage.getItem('value'));
+//   localStorage.setItem('value', 'theme-dark');
+//   body.classList.add(localStorage.getItem('value'));
+// };
+
+// lightBtn.addEventListener('click', handleLightClick);
+// darkBtn.addEventListener('click', handleDarkClick);
+
+// level 2
+/*
+ * Есть массив цветов в hex-формате и кнопки Start и Stop.
+ *
+ * Напиши скрипт, который после нажатия кнопки Start, раз в секунду
+ * меняет цвет фона body на случайное значение из массива. Используй
+ * инлайн-стиль для изменения background-color.
+ *
+ * При нажатии на кнопку Stop, изменении цвета фона должно останавливаться.
+ *
+ * Учти, что на кнопку Start можно нажать бесконечное количество раз.
+ * Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.
+ */
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+const timer = {
+  timerRun: false,
+  time: null,
+  randomNum() {
+    return Math.floor(Math.random() * 6);
+  },
+
+  handleStartBtnClick() {
+    if (this.timerRun) return;
+    this.timerRun = true;
+
+    this.time = setInterval(() => {
+      document.body.style.backgroundColor = colors[this.randomNum()];
+    }, 1000);
+  },
+
+  handleStopBtnClick() {
+    this.timerRun = false;
+
+    clearInterval(this.time);
+  },
+};
+
+refs.startBtn.addEventListener('click', timer.handleStartBtnClick.bind(timer));
+refs.stopBtn.addEventListener('click', timer.handleStopBtnClick.bind(timer));
---
+++ module-12/notepad-boilerplate-homework-12/src/js/utils/notepad-model.js
@@ -0,0 +1,99 @@
+const shortid = require('shortid');
+import { PRIORITIES } from './constants';
+
+export default class Notepad {
+  static getPriorityName(priorityId) {
+    return Notepad.PRIORITIES[priorityId].name;
+  }
+
+  constructor(notes = []) {
+    this._notes = notes;
+  }
+  get notes() {
+    return this._notes;
+  }
+  findNoteById(id) {
+    const note = this._notes.find(note => note.id === id);
+    console.log(note);
+
+    return note;
+  }
+  saveNote(note) {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => {
+        note.id = shortid.generate();
+        this._notes.push(note);
+
+        localStorage.setItem('notes', JSON.stringify(this._notes));
+        resolve(note);
+      }, 300),
+    );
+  }
+  loadNotes() {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => {
+        const notesToLoad = localStorage.getItem('notes');
+        if (!notesToLoad) return;
+
+        JSON.parse(notesToLoad).map(note => {
+          if (this.findNoteById(note.id)) return;
+          this.saveNote(note);
+        });
+
+        resolve(JSON.parse(notesToLoad));
+      }, 300),
+    );
+  }
+  deleteNote(id) {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => {
+        this._notes = this._notes.filter(note => note.id !== id);
+        localStorage.setItem('notes', JSON.stringify(this._notes));
+
+        resolve(this._notes);
+      }, 300),
+    );
+  }
+  updateNoteContent(id, updatedContent) {
+    const note = this.findNoteById(id);
+    if (!note) return;
+    const keys = Object.keys(updatedContent);
+    for (const key of keys) {
+      note[key] = updatedContent[key];
+    }
+    return note;
+  }
+  updateNotePriority(id, priority) {
+    const note = this.findNoteById(id);
+    if (!note) return;
+    note.priority = priority;
+    return note;
+  }
+  filterNotesByQuery(query) {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => {
+        const filteredNotes = [];
+        for (const note of this._notes) {
+          const { title, body } = note;
+          const noteContent = `${title} ${body}`;
+          const hasQuery = noteContent
+            .toLowerCase()
+            .includes(query.toLowerCase());
+          if (hasQuery) {
+            filteredNotes.push(note);
+          }
+        }
+        resolve(filteredNotes);
+      }, 300),
+    );
+  }
+  filterNotesByPriority(priority) {
+    const filteredNotes = this._notes.filter(
+      note => note.priority === priority,
+    );
+
+    return filteredNotes;
+  }
+}
+
+Notepad.PRIORITIES = PRIORITIES;
